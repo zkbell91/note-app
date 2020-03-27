cd02765d7d29a0b2789e47345009ed18d7a2a1be
zkbell91 <z.bell@me.com>
2020-03-27 17:09:44 +0000

    set axios HTTP patch request for onSubmit method to update existing notes

--- src/App.js
+++ src/App.js
@@ -34,9 +34,16 @@ class App extends Component {
     .catch((err) => console.log(err.response.data) );
   }
   
-  submitNote = (data) => {
-    console.log(data);
-    axios.post(urlFor(`notes`), data)
+  performSubmissionRequest = (data, id) => {
+    if (id) {
+      return axios.patch(urlFor(`notes/${id}`), data);
+    } else {
+      return axios.post(urlFor('notes'), data);
+    }
+  }
+  
+  submitNote = (data, id) => {
+    this.performSubmissionRequest(data, id)
     .then((res) => this.setState({ showNote: false}) )
     .catch((err) => console.log(err.response.data) );
   }
