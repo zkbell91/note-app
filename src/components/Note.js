e669cf7d0e9d215f74f10435f89cb5ed6f94a27f
zkbell91 <z.bell@me.com>
2020-03-28 03:27:55 +0000

    allow tag form to close when it is clicked off of

--- src/components/Note.js
+++ src/components/Note.js
@@ -13,43 +13,65 @@ class Note extends React.Component {
 
   onTagSubmit(e) {
     e.preventDefault(e);
-    console.log(this.name.value);
+    const formData = {
+      name: this.name.value
+    };
+    this.props.submitTag(formData, this.props.note.id);
     this.props.closeTagForm();
   }
   
-  renderTagForm() {
-    if (!this.props.newTag) {
-      return (
-        <span>
-          Tag your note:
-          <i
-            className="tag-button material-icons"
-            onClick={() => this.props.showTagForm()}
-          >
-            add circle
-          </i>
-        </span>
-      );
-    } else {
-      return (
-        <form onSubmit={(e) => this.onTagSubmit(e)}>
-          <input
-            className="tag-input"
-            type="text"
-            placeholder="Tage Name..."
-            ref={(input) => this.name = input}
+  renderTagForm(note) {
+    if (note.id !== undefined) {
+      if (!this.props.newTag) {
+        return (
+          <span>
+            Tag your note:
+            <i
+              className="tag-button material-icons"
+              onClick={() => this.props.showTagForm()}
+            >
+              add circle
+            </i>
+          </span>
+        );
+      } else {
+        return (
+          <form onSubmit={(e) => this.onTagSubmit(e)}>
+            <input
+              className="tag-input"
+              type="text"
+              placeholder="Tag Name..."
+              ref={(input) => this.name = input}
             />
-        </form>
+          </form>
+        );
+      }
+    }
+  }
+
+  renderTags(note) {
+    if (note.tags) {
+      return note.tags.map((tag, index) =>
+      <div className="tag" key={index}>
+        <span className="delete">
+          <i className="material-icons">delete</i>
+        </span>
+        {tag.name}
+      </div>
       );
     }
   }
   
   render() {
-    const { note } = this.props;
+    const { note, closeTagForm } = this.props;
 
     return (
       <div className="note-container">
-        <form className="note-form" onSubmit={(e) => this.onSubmit(e)}>
+        <form
+          className="note-form"
+          onSubmit={(e) => this.onSubmit(e)}
+          onClick={() => closeTagForm()}
+        >
           <input
             className="note-title-input"
             type="text"
@@ -67,7 +89,10 @@ class Note extends React.Component {
         </form>
         <div className="tag-container">
           <div className="tag-button-container">
-            {this.renderTagForm()}
+            {this.renderTagForm(note)}
+          </div>
+          <div className="tag-list-container">
+            {this.renderTags(note)}
           </div>
         </div>
       </div>
